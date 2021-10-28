e6b3861a0ba89e5156f4044b7fda3628c4b56354
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2021-10-27 19:27:41 -0700

    Added text display of data

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 /*
-Link javascript to HTML
-get access to button
-Get access to input field
+Render weather data to page:
+temp in F or C - high and low
+city name
+barometric pressure
+description
 
-create function to fetch API data when button is clicked
-
+Grab all 4 HTML elements
+Convert temp to C/F
+display data into 4 HTML elements
 */
 
 var inputFieldEl = document.querySelector("#city");
 var btn = document.querySelector("#get-weather");
+var tempEl = document.querySelector("#temp");
+var cityEl = document.querySelector("#city-name");
+var pressureEl = document.querySelector("#pressure");
+var descriptionEl = document.querySelector("#description");
 
 // Fetches data of a given city
 function fetchData() {
@@ -21,12 +28,29 @@ function fetchData() {
         .then(function (response) {
             return response.json();
         })
-        .then(function (data) {
-            console.log(data);
+        .then(function (weatherData) {
+            console.log(weatherData);
+
+            // Retrieving and converting temps
+            var highTemp = weatherData.main.temp_max;
+            highTemp = highTemp -273.15;
+            var lowTemp = weatherData.main.temp_min;
+            lowTemp = lowTemp -273.15;
+
+
+            // Text content
+            tempEl.textContent = "Highs of " + highTemp + " and lows of " + lowTemp + ".";
+            cityEl.textContent = weatherData.name;
+            pressureEl.textContent = "Extremely high pressure of: " + weatherData.main.pressure;
+            descriptionEl.textContent = weatherData.weather[0].description;
+            
         });
     
     inputFieldEl.value = "";
+}
 
+function displayTemp() {
+    
 }
 
 btn.addEventListener("click", fetchData);
